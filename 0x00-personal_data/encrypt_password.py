9a41ca2a5ab96381d7ff9e5d75a241749eadd029
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-09-12 21:57:04 +0100

    encrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""A module for encrypting passwords.
+"""
+
+
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password with bcrypt and return the salted, hashed password as a byte string.
+
+    Args:
+    password (str): The plain text password to hash.
+
+    Returns:
+    bytes: The salted, hashed password.
+    """
+    salt = bcrypt.gensalt()  # Generate a salt
+    hashed = bcrypt.hashpw(password.encode(), salt)  # Hash the password with the salt
+    return hashed
+
