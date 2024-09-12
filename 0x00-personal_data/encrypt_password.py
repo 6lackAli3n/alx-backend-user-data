2003ec90043a8922280a37c4c82019f1664d2922
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-09-12 22:00:18 +0100

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -19,3 +19,8 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode(), salt)  # Hash the password with the salt
     return hashed
 
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check if a given password matches the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
