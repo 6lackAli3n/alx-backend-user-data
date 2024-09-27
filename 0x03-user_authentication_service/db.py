483d7ff20919caf463f44d45f69e7f2bd2b27ef0
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-09-28 00:47:13 +0100

    db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import Optional
 
 from user import Base, User
 
